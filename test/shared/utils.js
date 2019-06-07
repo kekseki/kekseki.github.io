75a1c7bef1c68b067c578201d522a240f97407a5
damjan0 <47975020+damjan0@users.noreply.github.com>
2019-06-07 10:43:36 +0200

    test2

--- test/shared/utils.js
+++ test/shared/utils.js
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // remaps opacity from 0 to 1
 const opacityRemap = mat => {
   if (mat.opacity === 0) {
@@ -76,6 +61,9 @@ class Reticle extends THREE.Object3D {
    * @param {XRCoordinateSystem} frameOfRef
    */
   async update(frameOfRef) {
+    if (window.value==1) {
+      return;
+    }
     this.raycaster = this.raycaster || new THREE.Raycaster();
     this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
     const ray = this.raycaster.ray;
