8e2a570f3a1af0445789712a2b941f6b4506db0c
kekseki <33927250+kekseki@users.noreply.github.com>
2023-03-01 19:33:46 -0100

    Refactor and update content
    no more owl slider and bootstrap

--- res/js/main.js
+++ res/js/main.js
@@ -1,105 +1,64 @@
-var siteSliderRange = function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-	};
-	// siteSliderRange();
+/*----------------------------------------------*/
+//	Dark Mode
+/*-----------------------------------------------*/
 
-	var siteCarousel = function () {
-		if ( $('.nonloop-block-13').length > 0 ) {
-			$('.nonloop-block-13').owlCarousel({
-		    center: false,
-		    items: 1,
-		    loop: true,
-				stagePadding: 0,
-		    margin: 0,
-		    autoplay: true,
-		    nav: true,
-				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-		    responsive:{
-	        600:{
-	        	margin: 0,
-	        	nav: true,
-	          items: 2
-	        },
-	        1000:{
-	        	margin: 0,
-	        	stagePadding: 0,
-	        	nav: true,
-	          items: 3
-	        },
-	        1200:{
-	        	margin: 0,
-	        	stagePadding: 0,
-	        	nav: true,
-	          items: 4
-	        }
-		    }
-			});
-		}
+var darkMode = localStorage.getItem("darkMode");
+if (darkMode === "1") {
+  enableDarkMode();
+}
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-theme", "dark");
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+}
 
-		$('.slide-one-item').owlCarousel({
-	    items: 1,
-	    margin: 0,
-	    loop: true,
-	    center: false,
-	    mouseDrag: true,
-	    touchDrag: true,
-	        startPosition: 1,
+function goDark() {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode == "0") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+}
 
-	    nav: false,
-	    autoplay: true,
-	    autoplayTimeout: 7000,
-	    autoplayHoverPause: true,
-	    autoHeight: true,
-	    smartSpeed: 1000,
-	    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-	  });
+function enableDarkMode() {
+  document.documentElement.setAttribute("data-theme", "dark"); //enable
+  localStorage.setItem("darkMode", 1); //save to local storage
+}
 
-	  
-	};
-	siteCarousel();
+function disableDarkMode() {
+  document.documentElement.setAttribute("data-theme", "light");
+  //save to local storage
+  localStorage.setItem("darkMode", 0);
+}
 
 /*----------------------------------------------*/
-// Back to top button
+//	Slider
 /*-----------------------------------------------*/
-const toTop = document.querySelector(".to-top");
 
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 450) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
+var slidesM = document.getElementsByClassName("slide-mnenje");
+var dotsM = document.getElementsByClassName("dot-mnenje");
+var slidesJ = document.getElementsByClassName("slide-job");
+var dotsJ = document.getElementsByClassName("dot-job");
+selectSlide(slidesM, dotsM, 0); //select first slide
+selectSlide(slidesJ, dotsJ, 0); //select first slide
+
+//Dot triggers mnenje
+function showSlidesMnenje(n) {
+  selectSlide(slidesM, dotsM, n);
+}
+//Dot triggers job
+
+function showSlidesJob(n) {
+  selectSlide(slidesJ, dotsJ, n);
+}
+
+function selectSlide(slides, dots, n) {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].style.backgroundColor = "var(--hover)";
   }
-})
-
-/*----------------------------------------------*/
-// Hide buttons when scroll
-/*-----------------------------------------------*/
-const button = document.querySelector(".buttonOne");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 50) {
-    button.classList.add("hideBut");
-  } else {
-    button.classList.remove("hideBut");
-  }
-})
-
-const button2 = document.querySelector(".buttonTwo");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 50) {
-    button2.classList.add("hideBut");
-  } else {
-    button2.classList.remove("hideBut");
-  }
-})
+  slides[n].style.display = "block";
+  dots[n].style.backgroundColor = "var(--oposite-color)";
+}
